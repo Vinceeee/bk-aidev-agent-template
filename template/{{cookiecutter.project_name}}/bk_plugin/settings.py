acf553561956a2e5a7536b8945bbb4d903a1d1d0
Vinceeee <491323832@qq.com>
2025-05-08 20:32:04 +0800

    only for debug

--- template/{{cookiecutter.project_name}}/bk_plugin/settings.py
+++ template/{{cookiecutter.project_name}}/bk_plugin/settings.py
@@ -1,12 +1,6 @@
 import os
-from aidev_agent.api.utils import get_endpoint
-from aidev_agent.config import settings
 from blueapps.patch.settings_paas_services import STATICFILES_DIRS
 
 CUR_DIR = os.path.dirname(__file__)
 
 STATICFILES_DIRS += [os.path.join(CUR_DIR, "dist/static")]
-
-
-# 网关接口
-APIGW_ENDPOINT = get_endpoint(settings.APP_CODE, settings.BK_APIGW_STAGE)
