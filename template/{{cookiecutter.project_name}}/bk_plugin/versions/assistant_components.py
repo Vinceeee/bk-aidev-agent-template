34dceca8e7e14b9950380d4cddb50a00f7ceeb5e
b1ngsha <cheyne94@outlook.com>
2025-05-19 15:03:35 +0800

    feat: 新增非深度思考模型字段;更新版本号 # Reviewed, transaction id: 42839

--- template/{{cookiecutter.project_name}}/bk_plugin/versions/assistant_components.py
+++ template/{{cookiecutter.project_name}}/bk_plugin/versions/assistant_components.py
@@ -9,6 +9,9 @@ class PluginConfig(BaseModel):
     # 使用的 LLM（联系接口人获取可使用的 LLM 模型名称列表）。
     chat_model: str = "{{cookiecutter.chat_model}}"
 
+    # 非深度思考模型
+    non_thinking_llm: str = "{{cookiecutter.non_thinking_llm}}"
+
     # 在 AIDev 站点上传知识，然后将对应的知识库 ID 或者知识 ID 填在此处，可以在 agent 使用的时候检索对应范围的知识。
     # 通常来讲，选择的知识越多，检索速度也会越慢，检索效果也会越差。一般建议只选择该 agent 需要使用的知识，不要选择无关知识。
     knowledgebase_ids: List[int] = {{cookiecutter.knowledgebase_ids}} # 知识库 ID 的列表
